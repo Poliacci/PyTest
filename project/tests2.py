84d12865311d016d0af764e4c41e79acf5dd64ef
Maxim <max.polezhaev.01@mail.ru>
2023-08-08 23:22:56 +0300

    commit
    The real commit is actually here.
    In this commit I brought up the test for imported function sum.

--- project/tests2.py
+++ project/tests2.py
@@ -2,12 +2,12 @@ import unittest #framework
 from functions import sum
 d = [1, 2, 3]
 print (sum(d))
-# class TestSum(unittest.TestCase):
+class TestSum(unittest.TestCase):
 
-#     def test_list_int(self): # test that it can sum a list of integers
-#         data = [1, 2, 3]
-#         result = summary(data)
-#         self.assertEqual(result, 6)
+    def test_list_int(self): # test that it can sum a list of integers
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
 
-# if __name__ == '__main__': 
-#     unittest.main()
+if __name__ == '__main__': 
+    unittest.main()
